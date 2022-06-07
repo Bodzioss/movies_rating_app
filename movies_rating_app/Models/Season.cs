ddf1fc8e264616eb6c9ddffbf74bbe301f242f71
Bodzioss <marcin_bogdanski1@wp.pl>
2022-06-07 23:35:52 +0200

    Add Season model

---
+++ movies_rating_app/Models/Season.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesRatingApp.API.Models
+{
+    public class Season
+    {
+        public int ID { get; set; }
+        public int Number { get; set; }
+        [Required]
+        public int SeriesID { get; set; }
+    }
+}
