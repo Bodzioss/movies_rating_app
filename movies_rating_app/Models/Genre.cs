fc6bcbbf87ebe5b73f9a2f9670b5eb81cbc97887
Bodzioss <marcin_bogdanski1@wp.pl>
2022-06-07 23:34:51 +0200

    Add Genre model

---
+++ movies_rating_app/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesRatingApp.API.Models
+{
+    public class Genre
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Genre name cannot be longer than 50 characters.")]
+        public string? Name { get; set; }
+    }
+}
