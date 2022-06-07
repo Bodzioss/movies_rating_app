68d28f5edb281e20088df61245fc89770a3c9b8d
Bodzioss <marcin_bogdanski1@wp.pl>
2022-06-07 23:35:44 +0200

    Add Role model

---
+++ movies_rating_app/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesRatingApp.API.Models
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Role name cannot be longer than 50 characters.")]
+        public string? Name { get; set; }
+    }
+}
