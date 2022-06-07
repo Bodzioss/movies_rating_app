61738ccdddaecce4fe2b0eff33a0156bc148e8d2
Bodzioss <marcin_bogdanski1@wp.pl>
2022-06-07 21:09:21 +0200

    Add attributes to movie model

--- movies_rating_app/Models/Movie.cs
+++ movies_rating_app/Models/Movie.cs
@@ -7,14 +7,20 @@ namespace MoviesRatingApp.Models
     {
         public int ID { get; set; }
         [ForeignKey("Id")]
-        //public Director DirectorID { get; set; }
-        //public int GenreID { get; set; }
+        public int DirectorID { get; set; }
+        public int GenreID { get; set; }
+        [Required]
         [StringLength(50, ErrorMessage = "Title cannot be longer than 50 characters.")]
         public String? Title { get; set; }
+        [Required]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Year")]
         public DateTime ReleaseDate { get; set; }
 
+        [Required]
+        [Range(1,1000)]
+        [Display(Name = "Movie Length")]
         public int MovieLength { get; set; }
     }
 }
