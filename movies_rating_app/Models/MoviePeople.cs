7820219f8bd390423fda326754612ddc48e37881
Bodzioss <marcin_bogdanski1@wp.pl>
2022-06-07 23:35:38 +0200

    Add MoviePeople model

---
+++ movies_rating_app/Models/MoviePeople.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesRatingApp.API.Models
+{
+    public class MoviePeople
+    {
+        public int ID { get; set; }
+        [Required]
+        public int PersonID { get; set; }
+        [Required]
+        public int MovieID { get; set; }
+    }
+}
