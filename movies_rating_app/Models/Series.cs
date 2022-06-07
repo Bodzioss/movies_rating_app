35ca0905718a9b0c016e96aa3784812e55bb1170
Bodzioss <marcin_bogdanski1@wp.pl>
2022-06-07 23:36:02 +0200

    Add Series model

---
+++ movies_rating_app/Models/Series.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesRatingApp.API.Models
+{
+    public class Series
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Title cannot be longer than 50 characters.")]
+        public string? Title { get; set; }
+        [Required]
+        public int GenreID { get; set; }
+
+        [Range(1800,2100)]
+        [Display(Name = "Start Year")]
+        public int StartYear { get; set; }
+        [Range(1800, 2100)]
+        [Display(Name = "End Year")]
+        public int EndYear { get; set; }
+    }
+}
