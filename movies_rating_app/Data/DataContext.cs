56c079fbea167fe8755fefe01cfdf84686399ee2
Bodzioss <marcin_bogdanski1@wp.pl>
2022-06-07 20:44:01 +0200

    Create database connection

---
+++ movies_rating_app/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesRatingApp.Models;
+
+namespace MoviesRatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
