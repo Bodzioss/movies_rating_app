14751e317efc3f9b124da8fd9878aa5e085b57fa
Bodzioss <marcin_bogdanski1@wp.pl>
2022-06-24 20:31:25 +0200

    Add mapping profiles

--- movies_rating_app/MappingProfile.cs
+++ movies_rating_app/MappingProfile.cs
@@ -3,6 +3,13 @@ using Entities.Models;
 using Entities.DataTransferObjects;
 using Entities.DataTransferObjects.EpisodeDtos;
 using Entities.DataTransferObjects.GenreDtos;
+using Entities.DataTransferObjects.MovieGenreDtos;
+using Entities.DataTransferObjects.MoviePersonDtos;
+using Entities.DataTransferObjects.MovieDtos;
+using Entities.DataTransferObjects.PersonDtos;
+using Entities.DataTransferObjects.RoleDtos;
+using Entities.DataTransferObjects.SeasonDtos;
+using Entities.DataTransferObjects.SeriesDtos;
 
 namespace MoviesRatingApp.API
 {
@@ -15,6 +22,27 @@ namespace MoviesRatingApp.API
 
             CreateMap<Genre, GenreDto>();
             CreateMap<GenreForCreationDto, Genre>();
+
+            CreateMap<Movie, MovieDto>();
+            CreateMap<MovieForCreationDto, Movie>();
+
+            CreateMap<MovieGenre, MovieGenreDto>();
+            CreateMap<MovieGenreForCreationDto, MovieGenre>();
+
+            CreateMap<MoviePerson, MoviePersonDto>();
+            CreateMap<MoviePersonForCreationDto, MoviePerson>();
+
+            CreateMap<Person, PersonDto>();
+            CreateMap<PersonForCreationDto, Person>();
+
+            CreateMap<Role, RoleDto>();
+            CreateMap<RoleForCreationDto, Role>();
+
+            CreateMap<Season, SeasonDto>();
+            CreateMap<SeasonForCreationDto, Season>();
+
+            CreateMap<Series, SeriesDto>();
+            CreateMap<SeriesForCreationDto, Series>();
         }
     }
 }
